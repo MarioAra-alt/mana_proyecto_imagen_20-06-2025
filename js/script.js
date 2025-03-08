d7da661a986dd7b17e2e55db1453c7c030868d2c
MarioAra-alt <marioara795@gmail.com>
2025-03-07 18:40:46 -0600

    finalizacion de proyecto imagen con correcion del js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let imgs = [
 var originalX;
 var originalY;
 
-Window.onload = function(){
+window.onload = function(){
     document.onmousedown = startDrag;
     document.onmouseup = stopDrag;
 }
@@ -24,21 +24,22 @@ function sensorClick(){
 }
 
 function createPopUp(parent){
-    let p = document.getElementById("popup")
+    let p = document.getElementById("popup");
 
     if(p){
         p.parentNode.removeChild(p);
     }
-
-    let popup = document.createElement("div")
+    
+    let popup = document.createElement("div");
     popup.id = "popup";
     popup.className = "popup";
+    let rect = parent.getBoundingClientRect();
     popup.style.top = parent.y - 110 + "px";
     popup.style.left = parent.x -75 + "px";
 
-    let text =  document.createElement("span");
+    let text = document.createElement("span");
     text.textContent = parent.id;
-    popup.appendChild(popup);
+    popup.appendChild(text);
 
     var map = document.getElementsByClassName("map")[0];
     map.appendChild(popup);
@@ -47,19 +48,19 @@ function createPopUp(parent){
 function baseOnLoad(){
     var map = document.getElementsByClassName("map")[0];
     let base = document.getElementsByClassName("base")[0];
-    maxLeft = base.Width - 50;
+    maxLeft = base.width - 50;
     maxTop = base.height - 50;
 
     for(let i = 0; i < 6; i++){
-        let sensor =document.createElement("img");
+        let sensor = document.createElement("img");
         sensor.src = imgs[i%imgs.length];
         sensor.alt = i;
         sensor.id = i;
         sensor.draggable = true;
         sensor.classList.add("sensor");
         sensor.classList.add("dragme");
-        sensor.style.left = `${Math.floor(Math.random() * 900)}`;
-        sensor.style.top = `${Math.floor(Math.random() * 500)}`;
+        sensor.style.left = `${Math.floor(Math.random() * 900)}px`;
+        sensor.style.top = `${Math.floor(Math.random() * 500)}px`;
         sensor.onclick = sensorClick;
 
         let parent = document.getElementsByClassName("map")[0];
@@ -75,11 +76,11 @@ function startDrag(e){
     if(e.preventDefault) e.preventDefault();
 
     targ = e.target ? e.target : e.srcElement;
-
+    
     originalX = targ.style.left;
     originalY = targ.style.top;
 
-    if(!targ.classList.contains(`dragme`)) return;
+    if(!targ.classList.contains('dragme')) return;
 
     offsetX = e.clientX;
     offsetY = e.clientY;
@@ -91,20 +92,20 @@ function startDrag(e){
     return false;
 }
 
-function dragDiv(){
+function dragDiv(e){
     if(!drag) return;
     if(!e) var e = window.event;
 
     let newLeft = coordX + e.clientX - offsetX;
-    if(newLeft < maxLeft && newleft > minLeft) targ.style.left = newleft + `px`;
+    if(newLeft < maxLeft && newLeft > minLeft) targ.style.left = newLeft + 'px';
 
     let newTop = coordY + e.clientY - offsetY;
-    if(newTop < maxTop && newTop > minTop) targ.style.top = newTop + `px`
+    if(newTop < maxTop && newTop > minTop) targ.style.top = newTop + 'px';
     return false;
 }
 
 function stopDrag(){
-    if(typeof drag == `undefined`) return;
+    if(typeof drag == 'undefined') return;
     if(drag){
         if(Date.now() - timeDelta > 150){
             let p = document.getElementById("popup");
@@ -116,5 +117,5 @@ function stopDrag(){
             targ.style.top = originalY;
         }
     }
-    drag = false
+    drag = false;
 }
